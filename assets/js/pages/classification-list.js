c3b51bd5a3a0b11b07ea47fbac05b5ba2bc35ccd
Th@2.dev <99267273+Webww@users.noreply.github.com>
2023-11-11 21:03:42 +0700

    update classify page

--- assets/js/pages/classification-list.js
+++ assets/js/pages/classification-list.js
@@ -72,3 +72,38 @@ if (document.getElementById('authors-list')) {
         });
     });
 };
+
+// Validate 
+
+Validator({
+    form: '#form-add-category',
+    formGroupSelector: '.form-group',
+    errorSelector: '.form-message',
+    modal: '#form-modal-add-category',
+    rules: [
+        Validator.isRequired('.input-group input[name="category_name"]', '* Vui lòng nhập tên danh mục !'),
+    ],
+});
+
+Validator({
+    form: '#form-add-author',
+    formGroupSelector: '.form-group',
+    errorSelector: '.form-message',
+    modal: '#form-modal-add-author',
+    rules: [
+        Validator.isRequired('.input-group input[name="author_name"]', '* Vui lòng nhập tên tác giả !'),
+        Validator.isRequired('.input-group input[name="author_bio"]', '* Vui lòng nhập tiểu sử tác giả !'),
+    ]
+});
+
+Validator({
+    form: '#form-add-publisher',
+    formGroupSelector: '.form-group',
+    errorSelector: '.form-message',
+    modal: '#form-modal-add-publisher',
+    rules: [
+        Validator.isRequired('.input-group input[name="publisher_name"]', '* Vui lòng nhập tên nhà xuất bản !'),
+        Validator.isRequired('.input-group input[name="publisher_address"]', '* Vui lòng nhập địa chỉ nhà xuất bản !'),
+        Validator.isRequired('.input-group input[name="publisher_contact"]', '* Vui lòng nhập thông tin nhà xuất bản !'),
+    ]
+});
