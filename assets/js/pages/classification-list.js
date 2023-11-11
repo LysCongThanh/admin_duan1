26eaf41d678a22646908323ed7b463b4e3642f82
Th@2.dev <99267273+Webww@users.noreply.github.com>
2023-11-11 19:51:43 +0700

    separate necessary js files

---
+++ assets/js/pages/classification-list.js
@@ -0,0 +1,74 @@
+if (document.getElementById('products-list')) {
+    const dataTableSearch = new simpleDatatables.DataTable("#products-list", {
+        searchable: true,
+        fixedHeight: false,
+        perPage: 7
+    });
+
+    document.querySelectorAll(".export").forEach(function (el) {
+        el.addEventListener("click", function (e) {
+            var type = el.dataset.type;
+
+            var data = {
+                type: type,
+                filename: "soft-ui-" + type,
+            };
+
+            if (type === "csv") {
+                data.columnDelimiter = "|";
+            }
+
+            dataTableSearch.export(data);
+        });
+    });
+};
+
+if (document.getElementById('publisher-list')) {
+    const dataTableSearch = new simpleDatatables.DataTable("#publisher-list", {
+        searchable: true,
+        fixedHeight: false,
+        perPage: 7
+    });
+
+    document.querySelectorAll(".export-publisher-list").forEach(function (el) {
+        el.addEventListener("click", function (e) {
+            var type = el.dataset.type;
+
+            var data = {
+                type: type,
+                filename: "soft-ui-" + type,
+            };
+
+            if (type === "csv") {
+                data.columnDelimiter = "|";
+            }
+
+            dataTableSearch.export(data);
+        });
+    });
+};
+
+if (document.getElementById('authors-list')) {
+    const dataTableSearch = new simpleDatatables.DataTable("#authors-list", {
+        searchable: true,
+        fixedHeight: false,
+        perPage: 7
+    });
+
+    document.querySelectorAll(".export-authors-list").forEach(function (el) {
+        el.addEventListener("click", function (e) {
+            var type = el.dataset.type;
+
+            var data = {
+                type: type,
+                filename: "soft-ui-" + type,
+            };
+
+            if (type === "csv") {
+                data.columnDelimiter = "|";
+            }
+
+            dataTableSearch.export(data);
+        });
+    });
+};
